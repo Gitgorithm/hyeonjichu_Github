ebc09addb0e967b158caf52fd12e6ad6a48d5196
hyeonjiChu <guswl8280@naver.com>
2021-10-17 04:14:49 +0900

    BOJ

--- BOJ/BOJ_20055_컨베이어벨트위로봇.py
+++ BOJ/BOJ_20055_컨베이어벨트위로봇.py
@@ -7,22 +7,21 @@ ans = 0
 
 while True:
     ans += 1
-    tmp = queue.pop()
-    queue.appendleft(tmp)
-    tmp2 = robot.pop()
-    robot.appendleft(tmp2)
+    #컨베이어 벨트를 회전시킨다.
+    queue.appendleft(queue.pop())
+    robot.appendleft(robot.pop())
     if any(robot):  #로봇이 있으면 이동하기
         if robot[n-1] == 1: #내리는 위치에 있으면 바로 내린다.
             robot[n-1] = 0
-        for i in range(n-2, -1, -1):
+        for i in range(n-2, -1, -1):    #하나씩 검사해서 이동 가능하면 이동시킨다.
             if robot[i] == 1 and robot[i+1] == 0 and queue[i+1] >= 1:
-                if i+1 != n-1:
+                if i+1 != n-1:  #이동한 위치가 내리는 위치이면 바로 내린다.
                     robot[i + 1] = 1
                 robot[i] = 0
                 queue[i+1] -= 1
-    if queue[0] != 0:
+    if queue[0] != 0:   #맨 앞에 로봇을 올리수 있으면 올린다.
         robot[0] = 1
         queue[0] -= 1
-    if queue.count(0) >= k:
+    if queue.count(0) >= k: #내구도가 0인 것이 k개 이상이면 중단한다.
         print(ans)
         break

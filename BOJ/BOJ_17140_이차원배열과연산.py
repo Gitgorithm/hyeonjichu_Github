7d392bdabfb76d369fb15c778647e87e1e8ea344
hyeonjiChu <guswl8280@naver.com>
2021-10-19 22:39:45 +0900

    BOJ

--- BOJ/BOJ_17140_이차원배열과연산.py
+++ BOJ/BOJ_17140_이차원배열과연산.py
@@ -3,11 +3,12 @@ from collections import Counter
 r, c, k = map(int, input().split())
 arr = [list(map(int, input().split())) for x in range(3)]
 
+#인덱스 하나씩 줄이기
 r -= 1
 c -= 1
 time = 0
 
-
+#조건에 맞게 정렬 시키는 함수
 def sort1():
     s = 0
     for i in range(len(arr)):
@@ -15,11 +16,12 @@ def sort1():
         value = list(Counter(arr[i]).values())
         tmp = []
         for j in range(len(key)):
-            if key[j] != 0:
+            if key[j] != 0: #0인 경우는 정렬하지 않음
                 tmp.append([key[j], value[j]])
         tmp.sort(key=lambda x: (x[1], x[0]))
         arr[i] = sum(tmp, [])
         s = max(len(arr[i]), s)
+    #가장 긴 길이에 맞춰서 0 채우기
     for i in range(len(arr)):
         if len(arr[i]) != s:
             n = s - len(arr[i])
@@ -27,15 +29,18 @@ def sort1():
 
 
 while 1:
+    #조건 맞으면 중단, 범위 맞지 않을 수 있음 indexError 조심하기
     if len(arr) > r and len(arr[0]) > c:
         if arr[r][c] == k:
             break
     time += 1
-    if time > 100:
+    if time > 100:  #100초가 지나면 안됨
         time = -1
         break
+    #행 정렬
     if len(arr) >= len(arr[0]):
         sort1()
+    #열 정렬
     else:
         arr = list(map(list, zip(*arr)))
         sort1()
